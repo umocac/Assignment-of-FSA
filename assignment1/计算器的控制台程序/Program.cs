a26b2bfd2ae6d80192815baca064bc33fbbe443b
umocac <17268595883@163.com>
2025-02-22 17:03:08 +0800

    Add files via upload

---
+++ assignment1/计算器的控制台程序/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace calculator01
+{
+    internal class Program
+    {
+        static void Main()
+        {   
+            Console.WriteLine("本程序为实现+ - * /运算的计算器");
+            bool icheck = true;
+            float numb1 = 0, numb2 = 0 ;
+            for(; icheck;)
+            {
+                try
+                {
+                    Console.WriteLine("请输入一个数字：");
+                    var temp1 = Console.ReadLine();
+                    numb1 = float.Parse(temp1);
+                    icheck = false; 
+                }
+                catch 
+                {
+                    Console.WriteLine("输入错误!");
+                }
+            }
+
+            icheck = true;
+            string temp="";
+            for (; icheck;)
+            {
+                Console.WriteLine("请输入一个支持的运算符：");
+                temp = Console.ReadLine();
+                switch (temp)
+                {
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                        icheck = false;
+                        break;
+                    default:
+                        Console.WriteLine("输入错误！");
+                        break;
+                }
+            }
+
+            icheck = true;
+            for (; icheck;)
+            {
+                try
+                {
+                    Console.WriteLine("请输入一个数字：");
+                    var temp1 = Console.ReadLine();
+                    numb2 = float.Parse(temp1);
+                    icheck = false;
+                    if (numb2 == 0)
+                    {
+                        icheck = true;
+                        Console.WriteLine("输入错误！");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("输入错误!");
+                }
+            }
+
+            float finaout = 0;
+            switch (temp)
+            {
+                case "+":
+                    finaout = numb1 + numb2;
+                    break;
+                case "-":
+                    finaout = numb1 - numb2;
+                    break;
+                case "*":
+                    finaout = numb1 * numb2;
+                    break;
+                case "/":
+                    finaout = numb1 / numb2;
+                    break;
+            }
+            Console.WriteLine("计算结果为：");
+            Console.WriteLine(finaout);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ assignment1/计算器的窗口应用/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string snum1 = textBox1.Text, snum2 = textBox2.Text;
+            double numb1 = 0, numb2 = 0;
+            try
+            {
+                numb1 = double.Parse(snum1);
+                numb2 = double.Parse(snum2);
+            }
+            catch
+            {
+                MessageBox.Show("请输入数字！");
+                return;
+            }
+            switch (listBox1.Text)
+            {
+                case "+":
+                    MessageBox.Show(numb1.ToString() + " + " + numb2.ToString() + " = " + (numb1 + numb2).ToString());
+                    break;
+                case "-":
+                    MessageBox.Show(numb1.ToString() + " - " + numb2.ToString() + " = " + (numb1 - numb2).ToString());
+                    break;
+                case "*":
+                    MessageBox.Show(numb1.ToString() + " * " + numb2.ToString() + " = " + (numb1 * numb2).ToString());
+                    break;
+                case "/":
+                    if (numb2 == 0)
+                    {
+                        MessageBox.Show("除数不能为0！");
+                    }
+                    else
+                    {
+                        MessageBox.Show(numb1.ToString() + " / " + numb2.ToString() + " = " + (numb1 / numb2).ToString());
+                    }
+                    break;
+            }
+        }
+    }
+}
