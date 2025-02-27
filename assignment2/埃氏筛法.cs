46eddec6586df83ce9ca1b9194356d6a91ca6543
umocac <17268595883@163.com>
2025-02-27 15:44:51 +0800

    Add files via upload

---
+++ assignment2/埃氏筛法.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//埃氏筛法求素数
+namespace aissf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>();
+            for(int i = 2; i < 101; i += 1)
+            {
+                ints.Add(i);
+            }
+            for(int i = 0; i < ints.Count; i += 1)
+            {
+                int j = 2;
+                while (ints[i] * j < 101)
+                {
+                    ints.Remove(ints[i] * j);
+                    j += 1;
+                }
+            }
+            InfOut(ints);
+        }
+        static void InfOut(List<int> ints)
+        {
+            Console.WriteLine("2-100的素数如下：");
+            for (int i = 0; i < ints.Count; i += 1)
+            {
+                Console.Write(ints[i]);
+                Console.Write(" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ assignment2/托普利茨矩阵.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace tplz_matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\新建文件夹\vvss\tplz-matrix\matrix.txt";//"矩阵数据路径"
+            string content = File.ReadAllText(path);
+            Console.WriteLine(content);
+
+            if (JudgeMatrix(content))
+            {
+                Console.WriteLine("这是一个托普利茨矩阵");
+            }
+            else
+            {
+                Console.WriteLine("这不是一个托普利茨矩阵");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool JudgeMatrix(string content)
+        {
+            List<List<string>> list = new List<List<string>>();
+            int row = 0;
+            content = content.Replace("\r\n", "\n");
+            string[] rows=content.Split('\n');
+            foreach(string erow in rows)
+            {
+                if (erow == "")
+                {
+                    continue;
+                }
+                list.Add(new List<string>());
+                foreach (string estr in erow.Split(' '))
+                {
+                    if (estr != "")
+                    {
+                        list[row].Add(estr);
+                    }
+                }
+                row += 1;
+
+            }
+
+            if (list.Count == 1 || list[0].Count == 1)
+            {
+                return true;
+            }
+
+            int line = 0;
+            for (line = 0; line < list[0].Count; line += 1)
+            {
+                row = 0;
+                int tl = line;
+                string temp = list[row][line];
+                while (row < list.Count && tl < list[0].Count)
+                {
+                    if (temp == list[row][tl])
+                    {
+                        row += 1;
+                        tl += 1;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            for (row = 0; row < list.Count; row += 1)
+            {
+                line = 0;
+                int tr = row;
+                string temp = list[row][line];
+                while (tr < list.Count && line < list[0].Count)
+                {
+                    if (temp == list[tr][line])
+                    {
+                        tr += 1;
+                        line += 1;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
