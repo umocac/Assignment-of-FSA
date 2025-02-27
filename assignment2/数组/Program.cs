7ba585329e33620588cea8216ae990ebdde5d799
umocac <17268595883@163.com>
2025-02-27 22:30:08 +0800

    Add files via upload

---
+++ assignment2/数组/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace int_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //指定的数组↓
+            double[] arr = { 1, 2, 3, 4, 5, 6, 8 };
+            Console.Write("数组的最大值 = ");    Console.WriteLine(arr.Max());
+            Console.Write("数组的最小值 = ");    Console.WriteLine(arr.Min());
+            Console.Write("  数组的均值 = ");    Console.WriteLine((double)arr.Sum() / arr.Length);
+            Console.Write("数组元素的和 = ");    Console.WriteLine(arr.Sum());
+            Console.ReadKey();
+        }
+
+    }
+}
