5df6c7bb36bb445c0d9ce4cee5f8904672ce7eaf
umocac <17268595883@163.com>
2025-03-13 20:48:54 +0800

    Add files via upload

---
+++ assignment4/List_Foreach/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<double> list = new GenericList<double>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+
+            PrintList(list);
+            ListInf(list);
+
+            Console.ReadKey();
+        }
+
+        public class Node<T>{
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T d) {
+                this.Data = d;
+                this.Next = null;
+            }
+        }
+
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public GenericList()
+            {
+                head = tail = null;
+            }
+            public Node<T> Head()
+            {
+                return head;
+            }
+            public void Add(T d)
+            {
+                Node<T> n = new Node<T>(d);
+                if(tail == null) {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void Foreach(Action<T> action)
+            {
+                Node<T> n = head;
+                while(n != null)
+                {
+                    action(n.Data);
+                    n = n.Next;
+                }
+            }
+        }
+
+        public static void PrintList<T>(GenericList<T> list)
+        {
+            list.Foreach(n => Console.Write(n + " "));
+            Console.WriteLine();
+        }
+        public static T ListMax<T>(GenericList<T> list) where T : IComparable<T>
+        {
+            if (list.Head() == null)
+            {
+                throw new InvalidOperationException("链表为空！");
+            }
+            T max = list.Head().Data;
+            list.Foreach(d => { if (d.CompareTo(max) > 0) { max = d; } });
+            return max;
+        }
+        public static T ListMin<T>(GenericList<T> list) where T : IComparable<T>
+        {
+            if(list.Head() == null)
+            {
+                throw new InvalidOperationException("链表为空！");
+            }
+            T min=list.Head().Data;
+            list.Foreach(d => { if (d.CompareTo(min) < 0) { min = d; } });
+            return min;
+        }
+        public static T ListSum<T>(GenericList<T> list) 
+        {
+            dynamic sum = default(T);
+            list.Foreach(d => { sum += d; });
+            return sum; 
+        }
+
+        public static void ListInf<T>(GenericList<T> list)where T: IComparable<T>
+        {
+            try
+            {
+                Console.WriteLine("链表的最大值 = " + ListMax(list));
+                Console.WriteLine("链表的最小值 = " + ListMin(list));
+                Console.WriteLine("链表的和 = " + ListSum(list));
+            }
+            catch(InvalidOperationException)
+            {
+                Console.WriteLine("链表为空！");
+            }
+        }
+    }
+}
---
+++ assignment4/MyClock/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClock
+{
+    internal class Program
+    {
+        public delegate void ClockHandler(object sender, ClockEventArg arg);
+
+        public class ClockEventArg
+        {
+            public int h { get; set; }
+            public int m { get; set; }
+            public int ah {  get; set; }
+            public int am { get; set; }
+        }
+
+        public class Clock
+        {
+            public event ClockHandler run;
+            ClockEventArg arg = new ClockEventArg();
+            public Clock(int h, int m, int ah, int am)
+            {
+                arg.h = h;
+                arg.m = m;
+                arg.ah = ah;
+                arg.am = am;
+            }
+            public void Running()
+            {
+                arg.h = (arg.h + arg.m / 59) % 24;
+                arg.m = (arg.m + 1) % 60;
+                run(this, arg);
+            }
+            public void GetTime(out int h, out int m)
+            {
+                h = arg.h;
+                m = arg.m;
+            }
+            public bool SetTime(int h, int m)
+            {
+                if (h > 23 || m > 59)
+                {
+                    return false;
+                }
+                arg.h = h;
+                arg.m = m;
+                return true;
+            }
+            public bool SetAlarmT(int ah,int am)
+            {
+                if (ah > 23 || am > 59)
+                {
+                    return false;
+                }
+                arg.ah = ah;
+                arg.am = am;
+                return true;
+            }
+        }
+
+        public class Form
+        {
+            public Clock clock1 = new Clock(0, 0, -1, -1);
+            public Form()
+            {
+                clock1.run += new ClockHandler(Tick);
+                clock1.run += ShowTime;
+                clock1.run += Alarm;
+            }
+            void Tick(object sender, ClockEventArg arg)
+            {
+                Console.WriteLine("Tick.");
+            }
+            void ShowTime(object sender, ClockEventArg arg)
+            {
+                int h, m;
+                clock1.GetTime(out h, out m);
+                Console.WriteLine($"time = {h}:{m}");
+            }
+            void Alarm(object sender, ClockEventArg arg)
+            {
+                if (arg.m == arg.am && arg.h == arg.ah)
+                {
+                    Console.WriteLine("The clock is ringing.");
+                }
+
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            Form form= new Form();
+            form.clock1.SetAlarmT(0, 15);
+            for(int i = 0; i < 16; i += 1)
+            {
+                form.clock1.Running();
+                await Task.Delay(1000);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+    
+}
